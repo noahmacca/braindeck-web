4e35b4397cf6519adf288bfaebbc8dbe81e93458
Noah MacCallum <noahmacca@gmail.com>
2021-01-15 14:54:17 -0500

    clean up boilerplate and convert to ts

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Braindeck</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.title}>Braindeck</div>
+        <div className={styles.description}>A place to share your brain. Because you are the best discovery engine.</div>
+        <Link href="https://google.com">
+          <button className={styles.ctaButton}>
+            Enter
+          </button>
+        </Link>
+      </main>
+      <footer className={styles.footer}>
+        <div>
+          Feedback? Please{' '}
+          <a href="mailto:noahmacca@gmail.com"> send us an email.</a>
+        </div>
+      </footer>
+    </div>
+  )
+}
