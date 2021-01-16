e85ba003c2987f1995f866fcdef1fa4566e54222
Noah MacCallum <noahmacca@gmail.com>
2021-01-16 01:08:31 -0500

    Add head to navbar

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,6 +1,14 @@
+import Head from 'next/head'
+
 export default function NavBar() {
     return (
         <div>
+            <Head>
+                <title>Braindeck</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta property="og:title" content="BrainDeck" />
+                <meta property="og:description" content="Share your knowledge." />
+            </Head>
             NavBar
         </div>
     )
