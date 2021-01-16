5e4ca9b004eae572f779b75e5df87988398f71b5
Noah MacCallum <noahmacca@gmail.com>
2021-01-16 02:15:16 -0500

    placeholder graph display

--- pages/learn/sample.tsx
+++ pages/learn/sample.tsx
@@ -1,14 +1,13 @@
 import Head from '../../components/head';
 import NavBar from '../../components/navBar';
+import GraphDisplay from '../../components/graphDisplay'
 
 export default function Sample() {
   return (
     <div>
       <Head />
       <NavBar />
-      <main>
-        <div>Learning Path</div>
-      </main>
+      <GraphDisplay />
     </div>
   )
 }
