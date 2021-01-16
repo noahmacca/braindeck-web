8695c3c891167900e78b1c2e0c3db96f4c7d0129
Noah MacCallum <noahmacca@gmail.com>
2021-01-16 02:02:55 -0500

    Add buttons

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,12 +1,13 @@
 import styles from './navBar.module.css'
 
+
 export default function NavBar() {
     return (
         <div className={styles.container}>
             <div className={styles.navItemLeft}>
                 Noah's Learning Path
             </div>
-            <div className={styles.buttonRight}>
+            <div className={styles.buttonRight} onClick={() => alert('Coming Soon')}>
                 Login
             </div>
         </div>
