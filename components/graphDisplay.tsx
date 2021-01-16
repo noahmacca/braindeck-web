7a60cbef4ab233956caa17e89ebbd1ec840a1b7a
Noah MacCallum <noahmacca@gmail.com>
2021-01-16 14:09:29 -0500

    Convert landing page to bootstrap

--- components/graphDisplay.tsx
+++ components/graphDisplay.tsx
@@ -4,6 +4,10 @@ import { ArrowRight } from 'react-bootstrap-icons';
 export default function GraphDisplay() {
     return (
         <div className={styles.container}>
+            <h1>H1 title</h1>
+            <h3>H3 title  <small className="text-muted">With faded secondary text</small></h3>
+
+            <p>p tag content</p>
             <div className={styles.content}>
                 Here is a learning graph
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,28 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head />
-      <main className={styles.main}>
-        <div className={styles.title}>Braindeck</div>
-        <div className={styles.description}>A place to share your brain. Because you are the best discovery engine.</div>
-        <Link href="/learn/sample">
-        <button className={styles.ctaButton}>
-            Enter
-          </button>
-        </Link>
+      <main>
+        <div className="container d-flex flex-column align-content-center">
+          <div style={{ marginTop: '25vh' }} className="row text-center align-middle">
+            <div className="col">
+              <h1 className={styles.title}>Braindeck</h1>
+              <p className="lead">Become an expert in anything.</p>
+              <div className="d-grid gap-2 col-2 mx-auto my-5">
+                <Link href="/learn/sample"> 
+                  <button className="btn btn-primary" type="button">Enter</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
-      <footer className={styles.footer}>
-        <div>
-          Feedback? Please{' '}
-          <a href="mailto:noahmacca@gmail.com"> send us an email.</a>
+      <footer className="navbar fixed-bottom navbar-light py-3 bg-light">
+        <div className="container">
+          <span className="text-muted">Tweet or message <a href="https://twitter.com/noahmacca">@noahmacca</a> with feedback.</span>
         </div>
       </footer>
-    </div>
+    </>
   )
 }
