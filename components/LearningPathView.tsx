f9bd3fa95c317b4c7897829a2939fc05aa2b32de
Noah MacCallum <noahmacca@gmail.com>
2021-01-16 22:30:27 -0500

    Render learning path

--- components/LearningPathView.tsx
+++ components/LearningPathView.tsx
@@ -1,14 +1,44 @@
 
+function renderConcept(lpContents, iParent) {
+    return lpContents.map((content, i) => {
+        return (
+            <div className="container p-3">
+                <div key={`${content.id}`}>
+                    <a href={`${content.url}`}>
+                        <h5>{`${iParent+1}.${i+1}. ${content.title}`}</h5>
+                    </a>
+                    <div>{content.author}</div>
+                    <div>{content.takeaway}</div>
+                    <div>{content.format} {content.difficulty} {content.highlight}</div>
+                </div>
+            </div>
+        )
+    })
+}
+
+function renderConcepts(concepts) {
+    return concepts.map((concept, i) => {
+        return (
+            <div key={`${concept.id}`} className="container m-3">
+                <h2>{`${i+1}. ${concept.name}`}</h2>
+                { renderConcept(concept.contents, i) }
+            </div>
+        )
+    })
+}
+
 export default function LearningPathView({ learningPath }) {
+    const lpData = learningPath.data;
     return (
         <div>
             <div className="container mt-5">
                 <div className="row">
-                    <h2>{learningPath.data.title}</h2>
-                    <div>Created By: {learningPath.data.author.name}</div>
-                    <div>Background: {learningPath.data.background}</div>
-                    <div>Difficulty: {learningPath.data.difficulty}</div>
-                    <div>Estimated Time (hr): {learningPath.data.approxDurationHr}</div>
+                    <h1>{lpData.title}</h1>
+                    <div>Created By: {lpData.author.name}</div>
+                    <div>Background: {lpData.background}</div>
+                    <div>Difficulty: {lpData.difficulty}</div>
+                    <div>Estimated Time (hr): {lpData.approxDurationHr}</div>
+                    { renderConcepts(lpData.concepts) }
                 </div>
             </div>
         </div>
