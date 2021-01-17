bf4c6cc3b459816a05860965b2b9ec4a8cb51993
Noah MacCallum <noahmacca@gmail.com>
2021-01-16 22:06:09 -0500

    Render overall learning path info

--- components/LearningPathView.tsx
+++ components/LearningPathView.tsx
@@ -2,11 +2,14 @@
 export default function LearningPathView({ learningPath }) {
     return (
         <div>
-            <div className="alert alert-primary text-center">
-                New Learning Path Coming Soon
-            </div>
-            <div>
-                { JSON.stringify(learningPath) }
+            <div className="container mt-5">
+                <div className="row">
+                    <h2>{learningPath.data.title}</h2>
+                    <div>Created By: {learningPath.data.author.name}</div>
+                    <div>Background: {learningPath.data.background}</div>
+                    <div>Difficulty: {learningPath.data.difficulty}</div>
+                    <div>Estimated Time (hr): {learningPath.data.approxDurationHr}</div>
+                </div>
             </div>
         </div>
     )
