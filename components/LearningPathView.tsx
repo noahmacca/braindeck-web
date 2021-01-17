5f0883864109e363eedee46c2a96e75d5cd0a0b8
Noah MacCallum <noahmacca@gmail.com>
2021-01-16 22:43:27 -0500

    Improve rendering of learning path

--- components/LearningPathView.tsx
+++ components/LearningPathView.tsx
@@ -1,15 +1,21 @@
 
-function renderConcept(lpContents, iParent) {
-    return lpContents.map((content, i) => {
+function renderContents(contents, iParent) {
+    return contents.map((content, i) => {
         return (
-            <div className="container p-3">
-                <div key={`${content.id}`}>
+            <div key={`${content.id}-content`} className="container p-3">
+                <div>
                     <a href={`${content.url}`}>
-                        <h5>{`${iParent+1}.${i+1}. ${content.title}`}</h5>
+                        <h5>{`${iParent + 1}.${i + 1}. ${content.title}`}</h5>
                     </a>
-                    <div>{content.author}</div>
-                    <div>{content.takeaway}</div>
-                    <div>{content.format} {content.difficulty} {content.highlight}</div>
+                    <div>By {content.author}</div>
+                    {content.takeaway &&
+                        <div className="fst-italic">{content.takeaway}</div>
+                    }
+                    {content.highlight &&
+                        <div><span className="fw-bold">Highlight:</span> {content.highlight}</div>
+                    }
+                    <div><span className="fw-bold">Format:</span> {content.format}</div>
+                    <div><span className="fw-bold">Difficulty:</span> {content.difficulty}</div>
                 </div>
             </div>
         )
@@ -19,9 +25,9 @@ function renderConcept(lpContents, iParent) {
 function renderConcepts(concepts) {
     return concepts.map((concept, i) => {
         return (
-            <div key={`${concept.id}`} className="container m-3">
-                <h2>{`${i+1}. ${concept.name}`}</h2>
-                { renderConcept(concept.contents, i) }
+            <div key={`${concept.id}-concept`} className="container m-3">
+                <h2>{`${i + 1}. ${concept.name}`}</h2>
+                { renderContents(concept.contents, i)}
             </div>
         )
     })
@@ -31,14 +37,15 @@ export default function LearningPathView({ learningPath }) {
     const lpData = learningPath.data;
     return (
         <div>
-            <div className="container mt-5">
+            <div className="container m-5">
                 <div className="row">
                     <h1>{lpData.title}</h1>
-                    <div>Created By: {lpData.author.name}</div>
-                    <div>Background: {lpData.background}</div>
-                    <div>Difficulty: {lpData.difficulty}</div>
-                    <div>Estimated Time (hr): {lpData.approxDurationHr}</div>
-                    { renderConcepts(lpData.concepts) }
+                    <div><span className="fw-bold">Created By:</span> {lpData.author.name}</div>
+                    <div><span className="fw-bold">Learning Goal:</span> {lpData.learningGoal}</div>
+                    <div><span className="fw-bold">Background:</span> {lpData.background}</div>
+                    <div><span className="fw-bold">Overall Difficulty:</span> {lpData.difficulty}</div>
+                    <div><span className="fw-bold">Estimated Time:</span> {lpData.approxDurationHr} hr</div>
+                    {renderConcepts(lpData.concepts)}
                 </div>
             </div>
         </div>
