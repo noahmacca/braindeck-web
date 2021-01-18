4f5d98d59d46148d28b56dd34d5b6dfa544a4fe2
Noah MacCallum <noahmacca@gmail.com>
2021-01-18 16:35:49 -0500

    Learning path view style overview

--- components/LearningPathView.tsx
+++ components/LearningPathView.tsx
@@ -1,4 +1,4 @@
-import { HeartFill, Heart, CheckSquareFill, CheckSquare } from 'react-bootstrap-icons';
+import { HeartFill, Heart, CheckSquareFill, CheckSquare, Star, StarFill } from 'react-bootstrap-icons';
 import { useState } from 'react';
 import Link from 'next/link';
 
@@ -79,26 +79,63 @@ export default function LearningPathView({
 
   return (
     <div>
-      <div className="relative pt-6 px-4 sm:px-6 lg:px-8"></div>
-      <h1>{learningPath.title}</h1>
-      <h5>
-        {
-          lpHasFavorite ?
-            <HeartFill color="red" onClick={() => setLpHasFavorite(false)} /> :
-            <Heart color="red" onClick={() => setLpHasFavorite(true)} />
-        }{learningPath.countFavorite} {'   '}
-        {
-          isUserComplete ?
-            <CheckSquareFill color="green" /> :
-            <CheckSquareFill />
-        }{learningPath.countComplete}
-      </h5>
-      <div><span>Created By:</span> {learningPath.author.name}</div>
-      <div><span>Learning Goal:</span> {learningPath.learningGoal}</div>
-      <div><span>Background:</span> {learningPath.background}</div>
-      <div><span>Overall Difficulty:</span> {learningPath.difficulty}</div>
-      <div><span>Estimated Time:</span> {learningPath.approxDurationHr} hr</div>
-      {renderConcepts(learningPath.concepts, userData.contents)}
+      {/* stack boxes */}
+      <div className="relative bg-white overflow-hidden">
+        <div className="mx-auto px-6 max-w-4xl mt-10">
+          <div className="border bg-gray-50 rounded p-5 items-center text-gray-700">
+            <div className="text-sm text-gray-500">Machine Learning</div>
+            <div className="text-4xl pb-1 font-bold tracking-tight text-gray-800">{learningPath.title}</div>
+            <div className="flex text-md font-light text-gray-500">
+              <span className="flex pr-3">
+                <HeartFill className="px-1 text-red-500" size={24} />{parseInt(learningPath.countFavorite) + (lpHasFavorite ? 1 : 0)}
+              </span>
+              <span className="flex pr-3">
+                <StarFill className="text-yellow-300 mt-0.5" size={18}/>
+                <StarFill className="text-yellow-300 mt-0.5" size={18}/>
+                <StarFill className="text-yellow-300 mt-0.5" size={18}/>
+                <StarFill className="text-yellow-300 mt-0.5" size={18}/>
+                <Star className="text-yellow-300 mt-0.5" size={18}/>
+                <span className="pl-1">150</span>
+              </span>
+            </div>
+            <div className="px-5 py-3">
+              <div className="pb-3">
+                <div className="text-sm font-medium">Learning Goal</div>
+                <div className="text-md font-light">{learningPath.learningGoal}</div>
+              </div>
+              <div className="pb-3">
+                <div className="text-sm font-medium">Background Knowledge</div>
+                <div className="text-md font-light">{learningPath.background}</div>
+              </div>
+              <div className="pb-3">
+                <div className="text-sm font-medium">Created by <span className="font-light">{learningPath.author.name}</span></div>
+                <div className="text-sm font-medium">Last Updated <span className="font-light">Jan 2021</span></div>
+                <div className="text-sm font-medium">Difficulty <span className="font-light">{learningPath.difficulty}</span></div>
+                <div className="text-sm font-medium">Estimated Time <span className="font-light">{learningPath.approxDurationHr} hours</span></div>
+                <div className="text-sm font-medium">Progress <span className="font-light">90% complete</span></div>
+              </div>
+            </div>
+            {
+              !lpHasFavorite ?
+                <div className="border border-indigo-700 rounded p-1.5 flex w-36 text-indigo-700 bg-indigo-50 hover:bg-indigo-100" onClick={() => setLpHasFavorite(true)}>
+                  <div className="mx-auto flex">
+                    <span className="text-md">Favorite</span>
+                    <Heart className="ml-2 text-red-700 mt-1" size={20} />
+                  </div>
+                </div> :
+                <div className="border border-indigo-700 rounded p-1.5 flex w-36 text-indigo-700 bg-indigo-50 hover:bg-indigo-100" onClick={() => setLpHasFavorite(false)}>
+                  <div className="mx-auto flex">
+                    <span className="text-md">Favorited</span>
+                    <HeartFill className="ml-2 text-red-500 mt-1" size={20} />
+                  </div>
+                </div>
+            }
+          </div>
+        </div>
+      </div>
+      <div className="mt-20">
+        {renderConcepts(learningPath.concepts, userData.contents)}
+      </div>
     </div>
   )
 }
