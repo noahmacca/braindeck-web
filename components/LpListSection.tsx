f58d98d7e2d8373762db86d3a711d5584cc8897f
Noah MacCallum <noahmacca@gmail.com>
2021-01-19 16:00:42 -0500

    spacing

--- components/LpListSection.tsx
+++ components/LpListSection.tsx
@@ -3,10 +3,10 @@ import LearningPathSummary from './LearningPathSummary';
 export default function LpListSection({ title, lps, userData }) {
     const learningPath = lps[0].data
     return (
-        <div className="container my-5">
-            <div className="text-4xl pb-1 font-bold tracking-tight text-gray-800 capitalize">{title.toLowerCase()}</div>
+        <div className="container my-10">
+            <div className="text-4xl font-bold tracking-tight text-gray-800 capitalize">{title.toLowerCase()}</div>
             {lps.map((lp) => (
-                <div key={lp.id} className="m-2">
+                <div key={lp.id} className="mx-4 my-2">
                     <LearningPathSummary
                         learningPath={lp.data}
                         userData={userData}
