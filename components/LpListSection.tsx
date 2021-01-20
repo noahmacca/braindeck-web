2b3f686c284820f400cedc824a871b8714288c6d
Noah MacCallum <noahmacca@gmail.com>
2021-01-20 13:52:18 -0500

    explore learningsummary gray, clickable, show author

--- components/LpListSection.tsx
+++ components/LpListSection.tsx
@@ -1,4 +1,5 @@
 import LearningPathSummary from './LearningPathSummary';
+import Link from 'next/link';
 
 export default function LpListSection({ title, lps, userData }) {
     const learningPath = lps[0].data
@@ -7,11 +8,15 @@ export default function LpListSection({ title, lps, userData }) {
             <div className="text-2xl mb-2 md:mb-4 tracking-tight text-gray-800 capitalize">{title.toLowerCase()}</div>
             {lps.map((lp) => (
                 <div key={lp.id} className="md:mx-4 my-2">
-                    <LearningPathSummary
-                        learningPath={lp.data}
-                        userData={userData}
-                        isCompact
-                    />
+                    <Link href={`/learn/${lp.id}`}>
+                        <div className="cursor-pointer">
+                            <LearningPathSummary
+                                learningPath={lp.data}
+                                userData={userData}
+                                isCompact
+                            />
+                        </div>
+                    </Link>
                 </div>
             ))}
         </div>
