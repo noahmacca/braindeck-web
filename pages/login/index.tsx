10bf86c90b6fd9b2395260097e42d62849fa8c61
Noah MacCallum <noahmacca@gmail.com>
2021-01-23 23:26:44 -0500

    Add login and sign up forms

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,56 +1,25 @@
-import PageHead from "../../components/PageHead";
-import { useState, FormEvent } from 'react';
+import Link from 'next/link';
 
-export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        alert('Coming Soon!');
-        // api.login(email, password);
-    }
-
-    return (
-        <div>
-            <PageHead title="BrainDeck Login" />
-            <div className="relative bg-white overflow-hidden">
-                <div className="mx-auto px-6 mt-6 max-w-4xl">
-                    <div className="container mb-6 md:mb-10">
-                        <div className="my-5 text-3xl font-semibold">BrainDeck Login</div>
-                        <form onSubmit={handleSubmit}>
-                            <div className="mb-3">
-                                <label htmlFor="email">Email</label>
-                                <input
-                                    className="ml-2 rounded py-1 px-2 align-middle bg-gray-100"
-                                    type="email"
-                                    id="email"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="password">Password</label>
-                                <input
-                                    className="ml-2 rounded py-1 px-2 align-middle bg-gray-100"
-                                    type="password"
-                                    id="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </div>
-                            <div>
-                                <input
-                                    className="py-2 px-6 rounded-md bg-blue-600 text-gray-50"
-                                    type="submit"
-                                    id="submit"
-                                    value="Login"
-                                />
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center bg-gray-200">
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="text-center mt-24">
+       <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Log in</h2>
+       <p className="mt-2 text-center text-md text-gray-600">
+         {"Don't have an account? "}
+         <Link href="/signup">
+           <a href="#" className="text-blue-500">
+            Sign Up
+            </a>
+         </Link>
+       </p>
+     </div>
+      <div className="mt-8 bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+       TODO: Login Form
+      </div>
+    </div>
+  </div>
+ );
+};
+export default LoginPage;
