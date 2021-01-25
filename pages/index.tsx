db0a94d3b8153acabeffc6d5502cb0c545fb2d78
Noah MacCallum <noahmacca@gmail.com>
2021-01-24 22:23:22 -0500

    Update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,61 @@
 import PageHead from '../components/PageHead'
 import Link from 'next/link'
-// import styles from '../styles/Home.module.css'
+import { useAuth } from '../hooks/useAuth';
 
 export default function Home() {
-  return (
-    <>
-      <PageHead title="BrainDeck" />
-      <div className="relative bg-white overflow-hidden">
-        <div className="max-w-7xl mx-auto">
-          <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-            <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-              <polygon points="50,0 100,0 50,100 0,100" />
-            </svg>
-            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-              <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                  <div className="flex items-center justify-between w-full md:w-auto">
-                    <a href="#">
-                      <img className="h-8 w-auto sm:h-10" src="/bdLogo-md.jpeg" />
-                    </a>
-                  </div>
+    const auth = useAuth();
+    return (
+        <>
+            <PageHead title="BrainDeck" />
+            <div className="relative bg-white overflow-hidden">
+                <div className="max-w-7xl mx-auto">
+                    <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+                        <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+                            <polygon points="50,0 100,0 50,100 0,100" />
+                        </svg>
+                        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+                            <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
+                                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                                    <div className="flex items-center justify-between w-full md:w-auto">
+                                        <a href="#">
+                                            <img className="h-8 w-auto sm:h-10" src="/bdLogo-md.jpeg" />
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="block md:ml-10 md:pr-4 space-x-8">
+                                    <Link href="/explore">
+                                        <a className="font-medium text-gray-400 hover:text-gray-700 cursor-pointer">Explore</a>
+                                    </Link>
+                                    { !auth.user && <a href="#" className="font-medium text-indigo-600 hover:text-indigo-400">Log in</a> }
+                                </div>
+                            </nav>
+                        </div>
+                        <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                            <div className="sm:text-center lg:text-left">
+                                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                                    <span className="block">BrainDeck</span>
+                                    <span className="block text-indigo-600 text-3xl sm:text-4xl md:text-5xl">Learn Anything</span>
+                                </h1>
+                                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                    Ramp up on any topic with curated learning paths from the world's experts.
+                                </p>
+                                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                                    <div className="rounded-md shadow">
+                                        <Link href="/signup">
+                                            <a className="w-full flex items-center justify-center px-8 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-3 md:text-lg md:px-10 cursor-pointer">
+                                                Sign Up
+                                            </a>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </main>
+                    </div>
                 </div>
-                <div className="block md:ml-10 md:pr-4 space-x-8">
-                  <span className="font-medium text-gray-400 cursor-default">Learn More</span>
-                  <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Log in</a>
+                <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                    <div className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full hero-image-pattern"></div>
                 </div>
-              </nav>
             </div>
-            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-              <div className="sm:text-center lg:text-left">
-                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                  <span className="block">BrainDeck</span>
-                  <span className="block text-indigo-600 text-3xl sm:text-4xl md:text-5xl">Learn Anything</span>
-                </h1>
-                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  Ramp up on any topic with curated learning paths from the world's experts.
-                </p>
-                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                  <div className="rounded-md shadow">
-                    <Link href="/explore">
-                      <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                        Get started
-                      </a>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </main>
-          </div>
-        </div>
-        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <div className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full hero-image-pattern"></div>
-        </div>
-      </div>
-    </>
-  )
+        </>
+    )
 }
