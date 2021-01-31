fc1ab059a094ff1b4d0fdfe10b7067ed6d1c4843
Noah MacCallum <noahmacca@gmail.com>
2021-01-31 12:47:19 -0500

    update renderLearningResource

--- components/LearningPathView.tsx
+++ components/LearningPathView.tsx
@@ -2,44 +2,48 @@ import { CheckSquareFill, Check } from 'react-bootstrap-icons';
 import { useState } from 'react';
 import LearningPathSummary from './LearningPathSummary';
 import { useDb } from '../hooks/useDb';
-import { LearningPathUser, LearningConcept } from '../hooks/types';
+import { LearningPathUser, LearningConcept, LearningResource } from '../hooks/types';
 
-function renderContent(content, idx, isUserFavorite) {
-    const [isComplete, setIsComplete] = useState(isUserFavorite);
+function renderLearningResource(learningResource: LearningResource, idx: string) {
+    // const [isComplete, setIsComplete] = useState(isUserFavorite);
+    const db = useDb();
+    const isComplete = db.user.learningResources.some(uLr => (uLr.id === learningResource.id) && !!uLr.isCompleted);
+    console.log('renderLearningResource', isComplete)
+
     return (
         <div className="p-3 mx-3 bg-gray-50 mb-3 rounded-lg">
             <div className="text-xl mb-1">
-                {`${idx} `}<a href={`${content.url}`}>{`${content.title}`}</a>
+                {`${idx} `}<a href={`${learningResource.url}`}>{`${learningResource.title}`}</a>
             </div>
             <div className="text-sm mb-4">
-                {content.author}{' '}
+                {learningResource.author}{' '}
                 <span className="capitalize ml-2 text-xs p-1 rounded-lg bg-indigo-100 text-indigo-700 font-light">
-                    {content.format.toLowerCase()}
+                    {learningResource.format.toLowerCase()}
                 </span>{' '}
                 {
-                    content.difficulty === "EASY" ?
+                    learningResource.difficulty === "EASY" ?
                         <span className="capitalize text-xs p-1 ml-2 rounded-lg bg-green-100 text-green-700 font-light">
-                            {content.difficulty.toLowerCase()}
+                            {learningResource.difficulty.toLowerCase()}
                         </span> :
                         <span className="capitalize text-xs p-1 ml-2 rounded-lg bg-yellow-100 text-yellow-700 font-light">
-                            {content.difficulty.toLowerCase()}
+                            {learningResource.difficulty.toLowerCase()}
                         </span>
                 }
             </div>
             <div className="pb-2">
-                {content.takeaway && <div className="text-sm font-medium">Takeaway <span className="font-light">{content.takeaway}</span></div>}
-                {content.highlight && <div className="text-sm font-medium">Highlight <span className="font-light">{content.highlight}</span></div>}
+                {learningResource.description && <div className="text-sm font-medium">Description <span className="font-light">{learningResource.description}</span></div>}
+                {learningResource.highlight && <div className="text-sm font-medium">Highlight <span className="font-light">{learningResource.highlight}</span></div>}
             </div>
             <div>
                 {
                     !isComplete ?
-                        <div className="rounded p-1 mt-1 flex w-28 text-gray-700 bg-gray-100 hover:bg-gray-200 cursor-pointer" onClick={() => setIsComplete(true)}>
+                        <div className="rounded p-1 mt-1 flex w-28 text-gray-700 bg-gray-100 hover:bg-gray-200 cursor-pointer" onClick={() => alert(true)}>
                             <div className="mx-auto flex">
                                 <span className="text-sm">Complete</span>
                                 <Check className="ml-2 text-gray-500" size={20} />
                             </div>
                         </div> :
-                        <div className="rounded p-1 mt-1 flex w-28 text-green-700 bg-green-100 hover:bg-green-200 cursor-pointer" onClick={() => setIsComplete(false)}>
+                        <div className="rounded p-1 mt-1 flex w-28 text-green-700 bg-green-100 hover:bg-green-200 cursor-pointer" onClick={() => alert(false)}>
                             <div className="mx-auto flex">
                                 <span className="text-sm">Completed</span>
                                 <CheckSquareFill className="ml-2 text-green-500" size={20} />
@@ -52,7 +56,6 @@ function renderContent(content, idx, isUserFavorite) {
 }
 
 function renderLearningConcepts(learningConcepts: Array<LearningConcept>) {
-    const db = useDb();
     return learningConcepts.map((learningConcept, idxConcept) => {
         return (
             <div className="bg-white px-5 pt-5 items-center text-gray-700" key={`${learningConcept.id}-concept`}>
@@ -60,10 +63,10 @@ function renderLearningConcepts(learningConcepts: Array<LearningConcept>) {
                 <div>
                     {
                         learningConcept.learningResources.map((learningResource, idxResource) => {
-                            const isUserFavorite = db.user.learningResources.some(uLr => (uLr.id === learningResource.id) && !!uLr.isCompleted)
+                            
                             return (
                                 <div key={`${learningResource.id}-content`}>
-                                    { renderContent(learningResource, `${idxConcept + 1}.${idxResource + 1}.`, isUserFavorite)}
+                                    { renderLearningResource(learningResource, `${idxConcept + 1}.${idxResource + 1}.`)}
                                 </div>
                             )
                         })
@@ -77,9 +80,6 @@ function renderLearningConcepts(learningConcepts: Array<LearningConcept>) {
 export default function LearningPathView({ lpId }: { lpId: string }) {
     const db = useDb();
     const lp: LearningPathUser = db.userLearningPaths.filter((uLp) => uLp.id === lpId)[0]
-    const userLearningResources = db.user.learningResources;
-    console.log('LearningPathView, lp', lp);
-    console.log('LearningPathView, user', db.user);
     return (
         <div>
             <div className="relative bg-white overflow-hidden">
