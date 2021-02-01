067681edeb89221449d3b00fe0b514cee0fc64e5
Noah MacCallum <noahmacca@gmail.com>
2021-01-31 18:09:08 -0600

    My Courses tab to Favorites

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -49,7 +49,7 @@ export default function NavBar() {
                             </div> :
                             <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
                                 {NavTab('explore', router.pathname)}
-                                {NavTab('my courses', router.pathname)}
+                                {NavTab('favorites', router.pathname)}
                                 {NavTab('create', router.pathname)}
                                 {NavTab('profile', router.pathname)}
                                 <span onClick={() => auth.signOut()} className="px-3 py-2 rounded-md font-medium hover:text-indigo-400 text-indigo-600 cursor-pointer">Logout</span>
