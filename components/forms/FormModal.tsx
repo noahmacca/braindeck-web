830749f90a83763f9d6870683fc04527e473b749
Noah MacCallum <noahmacca@gmail.com>
2021-01-31 20:44:04 -0600

    Refactor FormModal into its own component

---
+++ components/forms/FormModal.tsx
@@ -0,0 +1,30 @@
+
+export default function FormModal({ title, shouldShowModal, dismissModal, children }) {
+    return (
+        <div className={`modal fixed w-full h-full top-0 left-0 flex items-center justify-center transition-opacity ease-in-out ${shouldShowModal ? null : 'opacity-0 pointer-events-none'}`}>
+            <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+
+            <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+
+                <div className="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                    <svg className="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                        <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                    </svg>
+                    <span className="text-sm">(Esc)</span>
+                </div>
+
+                <div className="modal-content py-4 text-left px-6">
+                    <div className="flex justify-between items-center pb-3">
+                        <p className="text-2xl font-bold">{title}</p>
+                        <div className="modal-close cursor-pointer z-50" onClick={() => dismissModal()}>
+                            <svg className="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                            </svg>
+                        </div>
+                    </div>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -6,42 +6,7 @@ import { useDb } from '../../hooks/useDb';
 import { useState, useEffect } from 'react';
 import LearningPathLoader from '../../components/LearningPathLoader'
 import NewLearningPathForm from '../../components/forms/NewLearningPathForm';
-
-const renderCreateLpModal = (shouldShowModal: boolean, setShouldShowCreateModal: Function) => {
-    return (
-        <div className={`modal fixed w-full h-full top-0 left-0 flex items-center justify-center transition-opacity ease-in-out ${shouldShowModal ? null : 'opacity-0 pointer-events-none'}`}>
-            <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
-
-            <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
-
-                <div className="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
-                    <svg className="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                        <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-                    </svg>
-                    <span className="text-sm">(Esc)</span>
-                </div>
-
-                <div className="modal-content py-4 text-left px-6">
-                    <div className="flex justify-between items-center pb-3">
-                        <p className="text-2xl font-bold">New Learning Path</p>
-                        <div className="modal-close cursor-pointer z-50" onClick={() => setShouldShowCreateModal(false)}>
-                            <svg className="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-                            </svg>
-                        </div>
-                    </div>
-
-                    <NewLearningPathForm
-                        dismiss={() => setShouldShowCreateModal(false)}
-                    />
-                </div>
-            </div>
-        </div>
-    )
-}
-
-
-
+import FormModal from '../../components/forms/FormModal';
 
 export default function CreateIndex() {
     const [shouldShowCreateModal, setShouldShowCreateModal] = useState(false);
@@ -82,7 +47,16 @@ export default function CreateIndex() {
                         </div>
                     </div>
                 </div>
-                {renderCreateLpModal(shouldShowCreateModal, setShouldShowCreateModal)}
+                <FormModal
+                    title="New Learning Path"
+                    shouldShowModal={shouldShowCreateModal}
+                    dismissModal={() => setShouldShowCreateModal(false)}
+                >
+                    <NewLearningPathForm
+                        dismiss={() => setShouldShowCreateModal(false)}
+                    />
+                </FormModal>
+                {/* {renderCreateLpModal(shouldShowCreateModal, setShouldShowCreateModal)} */}
             </LearningPathLoader>
         </div>
     )
