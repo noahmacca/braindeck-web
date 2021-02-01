9087dcc0a56cfcb1a211233a4a85c3170db22073
Noah MacCallum <noahmacca@gmail.com>
2021-01-31 19:56:36 -0600

    update form for overall learningPath data

--- components/forms/NewLearningPathForm.tsx
+++ components/forms/NewLearningPathForm.tsx
@@ -5,12 +5,6 @@ import { useState } from 'react';
 
 import Button from '../Button';
 
-interface SignUpData {
-    name: string;
-    email: string;
-    password: string;
-}
-
 const NewLearningPathForm: React.FC = () => {
     const auth = useAuth();
     const router = useRouter();
@@ -18,98 +12,142 @@ const NewLearningPathForm: React.FC = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const onSubmit = (data: SignUpData) => {
-        setIsLoading(true);
-        setError(null);
-        return auth.signUp(data).then((response) => {
-            setIsLoading(false);
-            response.error ? setError(response.error) : router.push('/create');
-        });
+    const onSubmit = (data) => {
+        console.log('onSubmit', data);
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="rounded-md shadow-sm">
-                <label
-                    htmlFor="name"
-                    className="block text-sm font-medium leading-5 text-gray-700"
-                >
-                    Name
+            <div className="rounded-md shadow-sm mb-3">
+                <label className="block text-sm font-medium leading-5 text-gray-700">
+                    URL
                 </label>
                 <input
-                    id="name"
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
                     type="text"
-                    name="name"
+                    name="url"
                     ref={register({
-                        required: 'Please enter a name',
+                        required: 'Please enter a url',
+                        pattern: {
+                            value: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+                            message: 'Not a valid url',
+                        },
                     })}
                 />
-                {errors.name && (
+                {errors.url && (
                     <div className="mt-2 text-xs text-red-600">
-                        {errors.name.message}
+                        {errors.url.message}
                     </div>
                 )}
             </div>
-            <div className="mt-6">
-                <label
-                    htmlFor="email"
-                    className="block text-sm font-medium leading-5 text-gray-700"
-                >
-                    Email address
+            <div className="rounded-md shadow-sm mb-3">
+                <label className="block text-sm font-medium leading-5 text-gray-700">
+                    Title
                 </label>
-                <div className="mt-1 rounded-md shadow-sm">
-                    <input
-                        id="email"
-                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                        type="email"
-                        name="email"
-                        ref={register({
-                            required: 'Please enter an email',
-                            pattern: {
-                                value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                                message: 'Not a valid email',
-                            },
-                        })}
-                    />
-                    {errors.email && (
-                        <div className="mt-2 text-xs text-red-600">
-                            {errors.email.message}
-                        </div>
-                    )}
-                </div>
+                <input
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+                    type="text"
+                    name="title"
+                    ref={register({
+                        required: 'Please enter a title',
+                    })}
+                />
+                {errors.title && (
+                    <div className="mt-2 text-xs text-red-600">
+                        {errors.title.message}
+                    </div>
+                )}
             </div>
-            <div className="mt-6">
-                <label
-                    htmlFor="password"
-                    className="block text-sm font-medium leading-5 text-gray-700"
-                >
-                    Password
+            <div className="rounded-md shadow-sm mb-3">
+                <label className="block text-sm font-medium leading-5 text-gray-700">
+                    Subject
                 </label>
-                <div className="mt-1 rounded-md shadow-sm">
-                    <input
-                        id="password"
-                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                        type="password"
-                        name="password"
-                        ref={register({
-                            required: 'Please enter a password',
-                            minLength: {
-                                value: 6,
-                                message: 'Should have at least 6 characters',
-                            },
-                        })}
-                    />
-                    {errors.password && (
-                        <div className="mt-2 text-xs text-red-600">
-                            {errors.password.message}
-                        </div>
-                    )}
-                </div>
+                <input
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+                    type="text"
+                    name="subject"
+                    ref={register({
+                        required: 'Please enter a subject',
+                    })}
+                />
+                {errors.subject && (
+                    <div className="mt-2 text-xs text-red-600">
+                        {errors.subject.message}
+                    </div>
+                )}
             </div>
-            <div className="mt-6">
-                <span className="block w-full rounded-md shadow-sm">
-                    <Button title="Sign Up" type="submit" isLoading={isLoading} />
+            <div className="rounded-md shadow-sm mb-3">
+                <label className="block text-sm font-medium leading-5 text-gray-700">
+                    Learning Goal
+                </label>
+                <input
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+                    type="text"
+                    name="learningGoal"
+                    ref={register({
+                        required: 'Please enter a learning goal',
+                    })}
+                />
+                {errors.learningGoal && (
+                    <div className="mt-2 text-xs text-red-600">
+                        {errors.learningGoal.message}
+                    </div>
+                )}
+            </div>
+            <div className="rounded-md shadow-sm mb-3">
+                <label className="block text-sm font-medium leading-5 text-gray-700">
+                    Background Knowledge
+                </label>
+                <input
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+                    type="text"
+                    name="background"
+                    ref={register({
+                        required: 'Please enter suggested background knowledge',
+                    })}
+                />
+                {errors.background && (
+                    <div className="mt-2 text-xs text-red-600">
+                        {errors.background.message}
+                    </div>
+                )}
+            </div>
+            <div className="rounded-md shadow-sm mb-3">
+                <label className="block text-sm font-medium leading-5 text-gray-700">
+                    Duration
+                </label>
+                <select name="duration" ref={register} className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
+                    <option value="Quick (<1 Hr)">Quick (&lt;1 Hr)</option>
+                    <option value="Fast (1-2 Hr)">Fast (1-2 Hr)</option>
+                    <option value="Medium (2-5 Hr)">Medium (2-5 Hr)</option>
+                    <option value="Long (5-10 Hr)">Long (5-10 Hr)</option>
+                    <option value="Very Long (>10 Hr)">Very Long (&gt;10 hr)</option>
+                </select>
+                {errors.duration && (
+                    <div className="mt-2 text-xs text-red-600">
+                        {errors.duration.message}
+                    </div>
+                )}
+            </div>
+            <div className="rounded-md shadow-sm mb-3">
+                <label className="block text-sm font-medium leading-5 text-gray-700">
+                    Difficulty
+                </label>
+                <select name="estDifficulty" ref={register} className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
+                    <option value="Easy">Easy</option>
+                    <option value="Moderate">Moderate</option>
+                    <option value="Hard">Hard</option>
+                    <option value="Advanced">Advanced</option>
+                </select>
+                {errors.estDifficulty && (
+                    <div className="mt-2 text-xs text-red-600">
+                        {errors.estDifficulty.message}
+                    </div>
+                )}
+            </div>
+            <div>
+                <span className="block w-full rounded-md shadow-sm mt-3">
+                    <Button title="Submit" type="submit" isLoading={isLoading} />
                 </span>
             </div>
             {error?.message && (
