b2bbf5f7adab66a8b77658c4b0a87cb4eff4413d
Noah MacCallum <noahmacca@gmail.com>
2021-02-02 15:31:50 -0800

    fixed order for explore page on load

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -3,6 +3,7 @@ import PageHead from "../../components/PageHead";
 import LpListSection from "../../components/LpListSection";
 import LearningPathLoader from '../../components/LearningPathLoader';
 import { useDb } from '../../hooks/useDb';
+import { useEffect } from 'react';
 import SectionHeader from "../../components/SectionHeader";
 
 const NUM_DISPLAY_LPS = 20;
@@ -21,7 +22,17 @@ export default function ExploreIndex({ subLpsArr }) {
     // Page layout
     const db = useDb();
     const uLps = db.userLearningPaths;
-    uLps.sort(compareByCountFavorites)
+
+    // uLps.sort(compareByCountFavorites);
+
+    let idOrder = uLps.map((uLp) => uLp.id);
+    useEffect(() => {
+        // Sort only on page load so they don't jump around on the user when they favorite
+        uLps.sort(compareByCountFavorites);
+        idOrder = uLps.map((uLp) => uLp.id);
+    }, []);
+
+    uLps.sort((a, b) => (idOrder.indexOf(a.id) - idOrder.indexOf(b.id)));
 
     return (
         <div>
