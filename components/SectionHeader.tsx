65ba4d17bb98f6369889ac44093fb27349398a14
Noah MacCallum <noahmacca@gmail.com>
2021-02-07 23:59:35 -0800

    sectionheader nicer

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -1,6 +1,6 @@
 
 const SectionHeader = ({text}: {text: string}) => (
-    <div className="md:text-center text-4xl mb-2 mt-6 md:mb-4 md:mt-10 tracking-tight font-semibold text-gray-700 capitalize">
+    <div className="md:text-center text-4xl mb-2 mt-6 md:mb-6 md:mt-12 tracking-tight font-light text-gray-700 capitalize">
         {text}
     </div>
 )
