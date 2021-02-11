a425ab541c381f18290c9607a47f4ab4a4196e58
Noah MacCallum <noahmacca@gmail.com>
2021-02-10 21:31:04 -0800

    styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
                                 </div>
                                 <div className="block md:ml-10 md:pr-4 space-x-8">
                                     <Link href="/explore">
-                                        <a className="font-medium text-gray-400 hover:text-gray-700 cursor-pointer">Explore</a>
+                                        <a className="font-medium text-gray-600 hover:text-gray-700 cursor-pointer">Explore</a>
                                     </Link>
                                     {!auth.userId && <a href="/login" className="font-medium text-indigo-600 hover:text-indigo-400">Log in</a>}
                                 </div>
@@ -58,7 +58,7 @@ export default function Home() {
                                     <div className="rounded-md shadow">
                                         <a href="http://cohort-signup.braindeck.io">
                                             <a className="w-full flex items-center justify-center px-8 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-3 md:text-lg md:px-10 cursor-pointer">
-                                                Apply
+                                                Apply for Cohort
                                             </a>
                                         </a>
                                     </div>
